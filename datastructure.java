1865814ed04b0d20deb55cf4de74466954ec0c3d
Vishnu Choubey <vishnuchoubey9939@gmail.com>
2025-02-05 17:49:28 +0530

    check subset sum

--- datastructure.java
+++ datastructure.java
@@ -1,16 +1,24 @@
 import java.util.*;
-class Student implements Comparable<Student>{
-          String name;
-          int age;
-          public Student(String name,int age){
-            this.name=name;
-            this.age=age;
-          }
-          public int compareTo();
-}
-public class datastructure {
-   public static void main(String args[]){
-   
 
-   } 
+public class datastructure {
+  public static boolean sum(int ind,int val,int array[]){
+    if(ind==0){
+      if(val==array[ind])return true;
+      return false;
+    }
+    
+    boolean nottake=sum(ind-1,val,array);
+    boolean take=false;
+    if(val>=array[ind]){
+      take=sum(ind-1,val-array[ind],array);
+    }
+    return take||nottake;
+  }
+  public static void main(String args[]){
+  int array[]={5,8,3,6,9,2};
+  int val=7;
+  System.out.println(sum(array.length-1,val,array));
+
+
+  }
 }
